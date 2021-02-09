3159c71d34ff75f27a7ab68a62fbc7476f9aefa5
Nikita <nikitabulaev3@gmail.com>
2021-02-09 16:52:18 +0400

    Реализация Dispose

--- WpfApp1/ViewModels/Base/ViewModel.cs
+++ WpfApp1/ViewModels/Base/ViewModel.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace WpfApp1.ViewModels.Base
 {
-    internal abstract class ViewModel : INotifyPropertyChanged
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -21,6 +21,17 @@ namespace WpfApp1.ViewModels.Base
             field = value;
             OnProperrtyChanged(PropertyName);
             return true;
-        } 
+        }
+
+        public void Dispose() 
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing) 
+        {
+            if (!Disposing || _Disposed) return;// Освобождение управляемых ресурсов
+        }
     }
 }
