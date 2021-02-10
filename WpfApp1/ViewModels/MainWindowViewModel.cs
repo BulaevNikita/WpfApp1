fe195dcf0826f5325368ddc3ef3d10f7fa8fdcaf
Nikita <nikitabulaev3@gmail.com>
2021-02-10 11:37:54 +0400

    тестирование работы модели в главном окне

--- WpfApp1/ViewModels/MainWindowViewModel.cs
+++ WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WpfApp1.ViewModels.Base;
 
 namespace WpfApp1.ViewModels
 {
-    class MainWindowViewModel
+    internal class MainWindowViewModel : ViewModel
     {
+        #region Заголовок окна
+        private string _Title = "Анализ статистики CV19";
+
+        public string Title 
+        {
+            get => _Title;
+            // set 
+            //{
+            //if (Equals(_Title, value)) return;
+            //_Title = value;
+            //OnProperrtyChanged();
+            //}
+            set => Set(ref _Title, value);
+        }
+        #endregion
     }
 }
